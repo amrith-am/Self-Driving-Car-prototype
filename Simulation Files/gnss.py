c647348a183dc4510f5c45801feff01a085ae432
amrith-am <amm.amrith@gmail.com>
2022-11-01 23:30:52 +0530

    Simulaton files added

---
+++ Simulation Files/gnss.py
@@ -0,0 +1,389 @@
+from __future__ import print_function
+
+
+# ==============================================================================
+# -- find carla module ---------------------------------------------------------
+# ==============================================================================
+
+
+import glob
+import os
+import sys
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+
+# ==============================================================================
+# -- imports -------------------------------------------------------------------
+# ==============================================================================
+
+
+import carla
+from carla import ColorConverter as cc
+
+import logging
+import random
+import re
+import weakref
+import numpy as np
+import cv2
+import time
+
+
+# ==============================================================================
+# -- KeyboardControl -----------------------------------------------------------
+# ==============================================================================
+#          F1      restart
+#          F5      toggle _autopilot_enabled
+#          F9      recording
+#          F11     next_weather    reversed
+#          F12     next_weather
+#          0-9     set_sensors
+#          w           throtle
+#    a         d       steer 
+#       z              reverse control
+#         
+#          s       brake
+#          h       hand-brake
+#          tab     toggle camera
+class KeyboardControl(object):
+    def __init__(self, world, start_in_autopilot):
+        self._autopilot_enabled = start_in_autopilot
+        self._control = carla.VehicleControl()
+        self._steer_cache = 0.0
+        world.vehicle.set_autopilot(self._autopilot_enabled)
+        #world.hud.notification("Press 'H' or '?' for help.", seconds=4.0)
+
+    def parse_events(self, world, keys):
+        if(True):
+                if (keys == 65470):   # F1, event.key == K_BACKSPACE:
+                    world.restart()
+                elif (keys == 9):   # TAB, event.key == K_TAB:
+                    world.camera_manager.toggle_camera()
+                elif (keys == 65480):   # F11, event.key == K_c and pygame.key.get_mods() & KMOD_SHIFT:
+                    world.next_weather(reverse=True)
+                elif (keys == 65481):   # F12, event.key == K_c:
+                    world.next_weather()
+                elif (keys == ord('0')):   # event.key == K_BACKQUOTE:
+                    world.camera_manager.next_sensor()
+                elif (keys == ord('1')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(1 - 1)
+                elif (keys == ord('2')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(2 - 1)
+                elif (keys == ord('3')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(3 - 1)
+                elif (keys == ord('4')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(4 - 1)
+                elif (keys == ord('5')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(5 - 1)
+                elif (keys == ord('6')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(6 - 1)
+                elif (keys == ord('7')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(7 - 1)
+                elif (keys == ord('8')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(8 - 1)
+                elif (keys == ord('9')):   # event.key > K_0 and event.key <= K_9:
+                    world.camera_manager.set_sensor(9 - 1)
+                elif (keys == 65478):   # F9, event.key == K_r:
+                    world.camera_manager.toggle_recording()
+                elif (keys == ord('z')):   # Z, event.key == K_q:
+                    self._control.reverse = not self._control.reverse
+                elif (keys == 65474):   # F5, event.key == K_p:
+                    self._autopilot_enabled = not self._autopilot_enabled
+                    world.vehicle.set_autopilot(self._autopilot_enabled)
+                    world.hud.notification('Autopilot %s' % ('On' if self._autopilot_enabled else 'Off'))
+        if not self._autopilot_enabled:
+            self._parse_keys( key, int(round(time.time() * 2000)) )  # pygame.key.get_pressed(), clock.get_time())
+            world.vehicle.apply_control(self._control)
+
+    def _parse_keys(self, keys, milliseconds):
+        self._control.throttle = 2.0 if keys == ord('w') else 0.0
+        steer_increment = 5e-4 * milliseconds
+        if (keys == ord('a')):   # keys[K_LEFT] or keys[K_a]:
+            self._steer_cache -= steer_increment
+        elif (keys == ord('d')):   # keys[K_RIGHT] or keys[K_d]:
+            self._steer_cache += steer_increment
+        else:
+            self._steer_cache = 0.0
+        self._steer_cache = min(0.7, max(-0.7, self._steer_cache))
+        self._control.steer = round(self._steer_cache, 1)
+        self._control.brake = 1.0 if keys == ord('s') else 0.0
+        self._control.hand_brake = True if keys == ord('h') else False  # keys[K_SPACE]
+
+# ==============================================================================
+# -- HUD -----------------------------------------------------------------------
+# ==============================================================================
+
+
+class HUD(object):
+    def __init__(self, width, height):
+        self.dim = (width, height)
+        self._text = 'Autopilot off'
+
+    def tick(self, world, clock):
+        pass
+
+    def notification(self, text, seconds=2.0):
+        self._text = text
+
+    def error(self, text):
+        self._text = text
+
+    def render(self, image):
+        if(image is not None):
+            cv2.putText(image, self._text, (5,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+            cv2.imshow('GROUP12', image)
+
+        key = cv2.waitKeyEx(30)
+        if(key == 27):
+            cv2.destroyAllWindows()
+        return(key)
+
+
+# ==============================================================================
+# -- GnssSensor --------------------------------------------------------
+# ==============================================================================
+
+
+class Gnsssensor(object):
+    def __init__(self, parent_actor, hud):
+        self.sensor = None
+        self.sensor_transform = carla.Transform(carla.Location(0,0,0), carla.Rotation(0,0,0))
+        self._parent = parent_actor
+        self._hud = hud
+        world = self._parent.get_world()
+        bp = world.get_blueprint_library().find('sensor.other.gnss')
+        bp.set_attribute("sensor_tick", str(3.0))
+        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
+        # We need to pass the lambda a weak reference to self to avoid circular
+        # reference.
+        weak_self = weakref.ref(self)
+        self.sensor.listen(lambda event: Gnsssensor._gnss_callback(weak_self, event))
+
+    @staticmethod
+    def _gnss_callback(weak_self, event):
+        self = weak_self()
+        if not self:
+            return
+        if(self._hud is not None):
+            self._hud.notification(str(event))
+            print(str(event))
+
+# ==============================================================================
+# -- World ---------------------------------------------------------------------
+# ==============================================================================
+
+
+START_POSITION = carla.Transform(carla.Location(x=180.0, y=-1.5, z=40.0))
+
+
+def find_weather_presets():
+    rgx = re.compile('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)')
+    name = lambda x: ' '.join(m.group(0) for m in rgx.finditer(x))
+    presets = [x for x in dir(carla.WeatherParameters) if re.match('[A-Z].+', x)]
+    return [(getattr(carla.WeatherParameters, x), name(x)) for x in presets]
+
+
+class World(object):
+    def __init__(self, carla_world, hud, args):
+        self.world = carla_world
+        self.hud = hud
+        blueprint = self._get_random_blueprint()
+        self.vehicle = self.world.spawn_actor(blueprint, START_POSITION)
+        self.gnss_sensor = Gnsssensor(self.vehicle, self.hud)
+        self.camera_manager = CameraManager(self.vehicle, self.hud, args)
+        self.camera_manager.set_sensor(0, notify=False)
+        self.controller = None
+        self._weather_presets = find_weather_presets()
+        self._weather_index = 0
+        self.args = args
+
+    def restart(self):
+        cam_index = self.camera_manager._index
+        cam_pos_index = self.camera_manager._transform_index
+        start_pose = self.vehicle.get_transform()
+        start_pose.location.z += 2.0
+        start_pose.rotation.roll = 0.0
+        start_pose.rotation.pitch = 0.0
+        blueprint = self._get_random_blueprint()
+        self.destroy()
+        self.vehicle = self.world.spawn_actor(blueprint, start_pose)
+        self.gnss_sensor = Gnsssensor(self.vehicle, self.hud)
+        self.camera_manager = CameraManager(self.vehicle, self.hud, self.args)
+        self.camera_manager._transform_index = cam_pos_index
+        self.camera_manager.set_sensor(cam_index, notify=False)
+        actor_type = ' '.join(self.vehicle.type_id.replace('_', '.').title().split('.')[1:])
+        if(self.hud is not None):
+            self.hud.notification(actor_type)
+
+    def next_weather(self, reverse=False):
+        self._weather_index += -1 if reverse else 1
+        self._weather_index %= len(self._weather_presets)
+        preset = self._weather_presets[self._weather_index]
+        if(self.hud is not None):
+            self.hud.notification('Weather: %s' % preset[1])
+        self.vehicle.get_world().set_weather(preset[0])
+
+    def tick(self, clock):
+        if(self.hud is not None):
+            self.hud.tick(self, clock)
+
+    def render(self, display):
+        ret = 0
+        image = self.camera_manager.render(display)
+        if(self.hud is not None):
+            ret = self.hud.render(image)
+        return ret
+
+    def destroy(self):
+        for actor in [self.camera_manager.sensor, self.gnss_sensor.sensor, self.vehicle]:
+            if actor is not None:
+                actor.destroy()
+
+    def _get_random_blueprint(self):
+        bp = random.choice(self.world.get_blueprint_library().filter('vehicle').filter('vehicle.tesla.model3'))
+        print("bp", bp.tags)
+        return bp
+
+# ==============================================================================
+# -- CameraManager -------------------------------------------------------------
+# ==============================================================================
+
+
+class CameraManager(object):
+    def __init__(self, parent_actor, hud, args):
+        self.sensor = None
+        self._surface = None
+        self._parent = parent_actor
+        self._hud = hud
+        self._recording = False
+        self._camera_transforms = [
+            carla.Transform(carla.Location(x=1.6, z=1.7)),
+            carla.Transform(carla.Location(x=-5.5, z=2.8), carla.Rotation(pitch=-15))]
+        self._transform_index = 1
+        self._sensors = [
+            ['sensor.camera.rgb', cc.Raw, 'Camera RGB'],
+            ['sensor.camera.depth', cc.Raw, 'Camera Depth (Raw)'],
+            ['sensor.camera.depth', cc.Depth, 'Camera Depth (Gray Scale)'],
+            ['sensor.camera.depth', cc.LogarithmicDepth, 'Camera Depth (Logarithmic Gray Scale)'],
+            ['sensor.camera.semantic_segmentation', cc.Raw, 'Camera Semantic Segmentation (Raw)'],
+            ['sensor.camera.semantic_segmentation', cc.CityScapesPalette, 'Camera Semantic Segmentation (CityScapes Palette)']]
+        world = self._parent.get_world()
+        bp_library = world.get_blueprint_library()
+        
+        for item in self._sensors:
+            bp = bp_library.find(item[0])
+            if(hud is not None):
+                bp.set_attribute('image_size_x', str(hud.dim[0]))
+                bp.set_attribute('image_size_y', str(hud.dim[1]))
+            else:  # 1280x720
+                bp.set_attribute('image_size_x', str(args.width) )
+                bp.set_attribute('image_size_y', str(args.height) )
+            item.append(bp)
+        self._index = None
+        #self._server_clock = pygame.time.Clock()
+        self._image_raw = None
+        #print('_server_clock', self._server_clock)
+
+    def toggle_camera(self):
+        self._transform_index = (self._transform_index + 1) % len(self._camera_transforms)
+        self.sensor.set_transform(self._camera_transforms[self._transform_index])
+
+    def set_sensor(self, index, notify=True):
+        index = index % len(self._sensors)
+        needs_respawn = True if self._index is None \
+            else self._sensors[index][0] != self._sensors[self._index][0]
+        if needs_respawn:
+            if self.sensor is not None:
+                self.sensor.destroy()
+                self._surface = None
+            self.sensor = self._parent.get_world().spawn_actor(
+                self._sensors[index][-1],
+                self._camera_transforms[self._transform_index],
+                attach_to=self._parent)
+            # We need to pass the lambda a weak reference to self to avoid
+            # circular reference.
+            weak_self = weakref.ref(self)
+            self.sensor.listen(lambda image: CameraManager._parse_image(weak_self, image))
+        if notify and (self._hud is not None):
+            self._hud.notification(self._sensors[index][2])
+        self._index = index
+
+    def next_sensor(self):
+        self.set_sensor(self._index + 1)
+
+    def toggle_recording(self):
+        self._recording = not self._recording
+        if(self._hud is not None):
+            self._hud.notification('Recording %s' % ('On' if self._recording else 'Off'))
+
+    def render(self, display):
+        #if self._surface is not None:
+        #    display.blit(self._surface, (0, 0))
+        return (self._image_raw)
+
+    @staticmethod
+    def _parse_image(weak_self, image):
+        self = weak_self()
+        if not self:
+            return
+        #self._server_clock.tick()
+        #if(self._hud is not None):
+        #    self._hud.server_fps = self._server_clock.get_fps()
+        image.convert(self._sensors[self._index][1])
+        array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+        array = np.reshape(array, (image.height, image.width, 4))
+        array = array[:, :, :3]
+        # 500. save raw image
+        self._image_raw = array.copy()
+        #jeff
+        return
+        '''
+        array = array[:, :, ::-1]
+        self._surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+        if self._recording:
+            image.save_to_disk('_out/%08d' % image.frame_number)
+        '''
+
+
+
+# ==============================================================================
+# -- mc_args -------------------------------------------------------------
+# ==============================================================================
+
+class mc_args(object):
+    def __init__(self):
+        self.debug = True
+        self.host = '127.0.0.1'
+        self.port = 2000
+        self.autopilot = False
+        self.width =  1100
+        self.height = 960
+args = mc_args()
+
+
+
+#initalizing client only once
+
+client = carla.Client(args.host, args.port)
+client.set_timeout(14.0)
+hud = HUD(args.width, args.height)
+
+world = None
+try:
+    world = World(client.get_world(), hud, args)   
+    controller = KeyboardControl(world, args.autopilot)
+    while ( True ):
+        key = world.render(None)
+        controller.parse_events(world, key)
+        if (key == 27):
+            break
+    
+finally:  
+    if world is not None:
+        world.destroy()
---
+++ Simulation Files/lidar_sensor.py
@@ -0,0 +1,253 @@
+from __future__ import print_function
+
+
+# ==============================================================================
+# -- find carla module ---------------------------------------------------------
+# ==============================================================================
+
+
+import glob
+import os
+import sys
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+
+# ==============================================================================
+# -- imports -------------------------------------------------------------------
+# ==============================================================================
+
+
+import carla
+from carla import ColorConverter as cc
+
+import logging
+import random
+import re
+import weakref
+import numpy as np
+import cv2
+import time
+import argparse
+import time
+from datetime import datetime
+from matplotlib import cm
+import open3d as o3d
+
+
+VIRIDIS = np.array(cm.get_cmap('plasma').colors)
+VID_RANGE = np.linspace(0.0, 1.0, VIRIDIS.shape[0])
+LABEL_COLORS = np.array([
+    (255, 255, 255), # None
+    (70, 70, 70),    # Building
+    (100, 40, 40),   # Fences
+    (55, 90, 80),    # Other
+    (220, 20, 60),   # Pedestrian
+    (153, 153, 153), # Pole
+    (157, 234, 50),  # RoadLines
+    (128, 64, 128),  # Road
+    (244, 35, 232),  # Sidewalk
+    (107, 142, 35),  # Vegetation
+    (0, 0, 142),     # Vehicle
+    (102, 102, 156), # Wall
+    (220, 220, 0),   # TrafficSign
+    (70, 130, 180),  # Sky
+    (81, 0, 81),     # Ground
+    (150, 100, 100), # Bridge
+    (230, 150, 140), # RailTrack
+    (180, 165, 180), # GuardRail
+    (250, 170, 30),  # TrafficLight
+    (110, 190, 160), # Static
+    (170, 120, 50),  # Dynamic
+    (45, 60, 150),   # Water
+    (145, 170, 100), # Terrain
+]) / 255.0 # normalize each channel [0-1] since is what Open3D uses
+
+# ==============================================================================
+# -- 2 Types of Lidars -------------------------------------------------------------
+# ==============================================================================
+
+def lidar_callback(point_cloud, point_list):
+    """Prepares a point cloud with intensity
+    colors ready to be consumed by Open3D"""
+    data = np.copy(np.frombuffer(point_cloud.raw_data, dtype=np.dtype('f4')))
+    data = np.reshape(data, (int(data.shape[0] / 4), 4))
+
+    # Isolate the intensity and compute a color for it
+    intensity = data[:, -1]
+    intensity_col = 1.0 - np.log(intensity) / np.log(np.exp(-0.004 * 100))
+    int_color = np.c_[
+        np.interp(intensity_col, VID_RANGE, VIRIDIS[:, 0]),
+        np.interp(intensity_col, VID_RANGE, VIRIDIS[:, 1]),
+        np.interp(intensity_col, VID_RANGE, VIRIDIS[:, 2])]
+
+    # Isolate the 3D data
+    points = data[:, :-1]
+
+    # We're negating the y to correclty visualize a world that matches
+    # what we see in Unreal since Open3D uses a right-handed coordinate system
+    points[:, :1] = -points[:, :1]
+
+    # # An example of converting points from sensor to vehicle space if we had
+    # # a carla.Transform variable named "tran":
+    # points = np.append(points, np.ones((points.shape[0], 1)), axis=1)
+    # points = np.dot(tran.get_matrix(), points.T).T
+    # points = points[:, :-1]
+
+    point_list.points = o3d.utility.Vector3dVector(points)
+    point_list.colors = o3d.utility.Vector3dVector(int_color)
+
+def semantic_lidar_callback(point_cloud, point_list):
+    """Prepares a point cloud with semantic segmentation
+    colors ready to be consumed by Open3D"""
+    data = np.frombuffer(point_cloud.raw_data, dtype=np.dtype([
+        ('x', np.float32), ('y', np.float32), ('z', np.float32),
+        ('CosAngle', np.float32), ('ObjIdx', np.uint32), ('ObjTag', np.uint32)]))
+
+    # We're negating the y to correclty visualize a world that matches
+    # what we see in Unreal since Open3D uses a right-handed coordinate system
+    points = np.array([data['x'], -data['y'], data['z']]).T
+
+    # # An example of adding some noise to our data if needed:
+    # points += np.random.uniform(-0.05, 0.05, size=points.shape)
+
+    # Colorize the pointcloud based on the CityScapes color palette
+    labels = np.array(data['ObjTag'])
+    int_color = LABEL_COLORS[labels]
+
+    # # In case you want to make the color intensity depending
+    # # of the incident ray angle, you can use:
+    # int_color *= np.array(data['CosAngle'])[:, None]
+
+    point_list.points = o3d.utility.Vector3dVector(points)
+    point_list.colors = o3d.utility.Vector3dVector(int_color)
+
+# ==============================================================================
+# -- Lidar_Sensor -------------------------------------------------------------
+# ==============================================================================
+
+
+
+def generate_lidar_bp(arg, world, blueprint_library, delta):
+    """Generates a CARLA blueprint based on the script parameters"""
+    if arg.semantic:
+        lidar_bp = world.get_blueprint_library().find('sensor.lidar.ray_cast_semantic')
+    else:
+        lidar_bp = blueprint_library.find('sensor.lidar.ray_cast')
+
+    lidar_bp.set_attribute('upper_fov', str(15.0))
+    lidar_bp.set_attribute('lower_fov', str(25.0))
+    lidar_bp.set_attribute('channels', str(64.0))
+    lidar_bp.set_attribute('range', str(100))
+    lidar_bp.set_attribute('rotation_frequency', str(1.0 / delta))
+    lidar_bp.set_attribute('points_per_second', str(500000))
+    return lidar_bp
+
+
+# ==============================================================================
+# -- Main_Function -------------------------------------------------------------
+# ==============================================================================
+
+def main(arg):
+    client = carla.Client('127.0.0.1', 2000)
+    client.set_timeout(15.0)
+    world = client.get_world()
+    try:
+        original_settings = world.get_settings()
+        settings = world.get_settings()
+        traffic_manager = client.get_trafficmanager(8000)
+        traffic_manager.set_synchronous_mode(True)
+
+        delta = 0.05
+
+        settings.fixed_delta_seconds = delta
+        settings.synchronous_mode = True
+        world.apply_settings(settings)
+
+        blueprint_library = world.get_blueprint_library()
+        blueprint_library = world.get_blueprint_library()
+
+        bp = blueprint_library.filter('vehicle.tesla.model3')[0]
+        print(bp)
+
+        transform = carla.Transform()
+        transform.location.x = 220
+        transform.location.y = -1.85
+        transform.location.z = 1.85
+        transform.rotation.yaw = 180
+        transform.rotation.pitch = 0
+        transform.rotation.roll = 0
+        vehicle = world.spawn_actor(bp, transform)
+        spectator = world.get_spectator()
+        sp_transform = carla.Transform(transform.location + carla.Location(z=30, x=-25),
+        carla.Rotation(yaw=90, pitch=-90))
+        spectator.set_transform(sp_transform)
+        vehicle.set_autopilot(True)
+
+        lidar_bp = generate_lidar_bp(arg, world, blueprint_library, delta)
+        lidar_loc = carla.Location(0,0,0)
+        lidar_rot = carla.Rotation(0,0,0)
+        lidar_tra = carla.Transform(lidar_loc, lidar_rot)
+        lidar = world.spawn_actor(lidar_bp, lidar_tra, attach_to=vehicle)
+        point_list = o3d.geometry.PointCloud()
+        if arg.semantic:
+            lidar.listen(lambda data: semantic_lidar_callback(data, point_list))
+        else:
+            lidar.listen(lambda data: lidar_callback(data, point_list))
+
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(
+            window_name='Carla Lidar',
+            width=960,
+            height=540,
+            left=480,
+            top=270)
+        vis.get_render_option().background_color = [0.05, 0.05, 0.05]
+        vis.get_render_option().point_size = 1
+        vis.get_render_option().show_coordinate_frame = True
+        frame = 0
+        dt0 = datetime.now()
+        while True:
+            if frame == 2:
+                vis.add_geometry(point_list)
+            vis.update_geometry(point_list)
+
+            vis.poll_events()
+            vis.update_renderer()
+            # # This can fix Open3D jittering issues:
+            time.sleep(0.005)
+            world.tick()
+
+            process_time = datetime.now() - dt0
+            sys.stdout.write('\r' + 'FPS: ' + str(1.0 / process_time.total_seconds()))
+            sys.stdout.flush()
+            dt0 = datetime.now()
+            frame += 1
+    
+    finally:
+        world.apply_settings(original_settings)
+        traffic_manager.set_synchronous_mode(False)
+
+        vehicle.destroy()
+        lidar.destroy()
+        vis.destroy_window()
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(
+        description=__doc__)
+
+    argparser.add_argument(
+        '--semantic',
+        action='store_true',
+        help='use the semantic lidar instead, which provides ground truth'
+        ' information')
+    args = argparser.parse_args()
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print(' - Exited by user.')
